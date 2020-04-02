380c7c575232a8f91366147a9538b6a164d320a7
Alan Yeung <alan_y987@hotmail.com>
2020-04-02 17:20:57 +0100

    remove github dependencies

--- inst/rmarkdown/templates/hps-stats-report/skeleton/create_report.R
+++ inst/rmarkdown/templates/hps-stats-report/skeleton/create_report.R
@@ -1,63 +1,45 @@
 # Compile report with table of contents and front cover included
 
-# PLEASE READ:
-# When the report has been compiled with the table of contents and cover page
-# included, there will be a warning when you open the report saying that the
-# document contains fields that may refer to other files: click Yes
-# If you want to get rid of this warning, re-save the file after opening it
 
-# Load required libraries
-library(officer)
-library(magrittr)
 
-# Parameters - This should be the only section that needs edited
-# Change as required for your publication
-# Note: filenames need to be filepaths if they are not stored in your R working
-# directory
+# Parameters --------------------------------------------------------------
+# Please change as required for your publication
+# Note: filenames need to be filepaths if they are not stored in your current R
+# working directory
+
 params <- list(
+  # Report filename - Please check carefully
   rmd_filename    = "Report.Rmd",
+  # Cover page filename - Please check carefully
   cover_filename  = "Cover_Page.docx",
+  # Title for cover page
   title           = "My Title",
+  # Subtitle for cover page
   subtitle        = "My Subtitle",
+  # Date of Publication
   date            = "DD Month YYYY",
+  # Output filename for compiled report
   filename_out    = "Report_and_Cover.docx"
 )
 
 
 
-# Create Report and Add Table of Contents ---------------------------------
+# Compile Report ----------------------------------------------------------
 
-rmarkdown::render(params$rmd_filename, output_file = "temp_report.docx",
-                  envir = new.env())
-
-read_docx("temp_report.docx") %>%
-  cursor_reach(keyword = "Introduction") %>%
-  body_add_break(pos = "before") %>%
-  body_add_toc() %>%
-  body_add_par("Contents", pos = "before", style = "Contents Header") %>%
-  print("temp_report2.docx")
+# Note: if source() below cannot find the 'compile.R' file, check that your
+# current working directory contains the 'compile.R' file
+# Check your current working directory with getwd()
+# If needed, set your working directory using setwd() to the directory that
+# contains 'compile.R'
+source("compile.R")
 
 
-
-# Cover Page --------------------------------------------------------------
-
-cover_page <- read_docx("Cover_Page.docx") %>%
-  body_replace_all_text("Insert publication title here", params$title) %>%
-  body_replace_all_text("Subtitle", params$subtitle) %>%
-  body_replace_all_text("DD Month YYYY", params$date)
-
-
-
-# Combine Cover and Report ------------------------------------------------
-
-cover_page %>%
-  body_add_break() %>%
-  cursor_end() %>%
-  body_add_docx("temp_report2.docx") %>%
-  print(params$filename_out)
-
-
-
-# Remove Temporary Files --------------------------------------------------
-
-unlink(c("temp_report.docx", "temp_report2.docx"))
+# PLEASE READ
+# When the report has been compiled with the table of contents and cover page
+# included, keep a note of the following:
+# In Word 2007, there may be a warning popup when you open the report saying that
+# the document contains fields that may refer to other files: click Yes
+# If you want to get rid of this warning, re-save the file after opening it
+# In Word 2016, there may be no warning popup but the table of contents may not
+# immediately appear - to make it appear go to the 'References' tab and click
+# 'Update Table'
