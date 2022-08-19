9e601dd3259a450314460e964f78f31b563149be
RosalynLP <rosalyn.pearson@phs.scot>
2022-08-19 07:46:09 +0100

    allow user to bypass Rstudio by setting append in function call

--- R/add_gitignore.R
+++ R/add_gitignore.R
@@ -1,13 +1,18 @@
 #' add_gitignore
-#' @description Add PHS template .gitignore file to chosen directory. This function is intended for use in R Studio.
+#' @description Add PHS template .gitignore file to chosen directory.
+#'
+#' @param path String: path to add .gitignore file. If left blank, will prompt the user within R Studio
+#' @param append Logical: if a .gitignore already exists, whether to append contents (TRUE) or overwrite (FALSE).
+#' If left blank will prompt the user within R Studio.
 #'
 #' @return Adds .gitignore file to directory, or appends content to existing file.
-
+#'
 #' @examples
 #' \dontrun{
 #' add_gitignore()
 #' }
-add_gitignore <- function(path=rstudioapi::selectDirectory(caption="Select folder to add .gitignore")) {
+add_gitignore <- function(path=rstudioapi::selectDirectory(caption="Select folder to add .gitignore"),
+                          append=NULL) {
 
   if(is.null(path)){
     return(message(".gitignore file not added."))
@@ -46,12 +51,16 @@ add_gitignore <- function(path=rstudioapi::selectDirectory(caption="Select folde
   gitignore <- paste(gitignore, collapse = "\n")
 
   # Search for existing gitignore in path
-  if(file.exists(paste0(path, ".gitignore"))){
-    append <- rstudioapi::showQuestion(title = "Append to existing .gitignore?",
-                                       message = "You already have a .gitignore file. Should I append the PHS gitignore or overwrite?",
-                                       "Append", "Overwrite")
+  if(file.exists(paste0(path, "/.gitignore"))){
+    # If append not set by user, prompt for whether to append
     if(is.null(append)){
-      return(message(".gitignore file not added. Please set correct directory and try again."))
+      append <- rstudioapi::showQuestion(title = "Append to existing .gitignore?",
+                                         message = "You already have a .gitignore file. Should I append the PHS gitignore or overwrite?",
+                                         "Append", "Overwrite")
+    }
+    # If user has still not set whether to append, don't add .gitignore
+    if(is.null(append)){
+      return(message(".gitignore file not added."))
     }
   } else {
     append <- FALSE
