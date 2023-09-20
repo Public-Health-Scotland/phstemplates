14e5d9a21be2e92cb2711488669b07908d5e231f
Alan Y <34042624+alan-y@users.noreply.github.com>
2023-09-20 11:41:30 +0100

    run_on function (#59)
    * run_on function
    
    * noRd for documentation
    
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>
    
    * remove captioner from suggests
    
    ---------
    
    Co-authored-by: Alan Yeung <alan.yeung@phs.scot>
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>

---
+++ R/run_on.R
@@ -0,0 +1,19 @@
+#' What system is the code being run on
+#'
+#' Collates RStudio and R version information into
+#' a character vector which can be used elsewhere.
+#'
+#' @param linebreak (Default: FALSE) - Should a 
+#' linebreak be appended to the 'run on' text?
+#'
+#' @noRd
+run_on <- function(linebreak = FALSE) {
+  out <- paste0(
+    "# Written/run on RStudio ",
+    rstudioapi::versionInfo()$mode, " ",
+    rstudioapi::versionInfo()$version, " and R ",
+    version$major, ".", version$minor
+  )
+
+  ifelse(linebreak, paste0(out, "\n"), out)
+}
