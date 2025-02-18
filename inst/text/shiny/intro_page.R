2068f0c4e2f644f0b99bf9f8e5638ce0c4766258
jasonm03 <jason.mcglasson2@phs.scot>
2025-02-18 16:59:50 +0000

    Change to H2 header to maintain consistent outline of the page for assistive technologies.

--- inst/text/shiny/intro_page.R
+++ inst/text/shiny/intro_page.R
@@ -4,8 +4,8 @@ output$intro_page_ui <-  renderUI({
 
   div(
 	     fluidRow(
-            h3("This is a header"),
-	           p("This is some text"), 
+            h2("This is a header"),
+	           p("This is some text"),
 	           p(strong("This is some bold text"))
 	      ) #fluidrow
    ) # div
