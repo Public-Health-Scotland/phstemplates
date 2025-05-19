3c28a4db88e526d4843a4fe9fc71547616181705
Alan Yeung <alan.yeung@phs.scot>
2025-05-19 16:47:39 +0100

    shiny app function changes

--- R/shiny_app_template.R
+++ R/shiny_app_template.R
@@ -16,24 +16,7 @@ shiny_app_template <- function(app_name = "WRITE APP NAME HERE",
     "# Get packages",
     'source("setup.R")',
     "",
-    "# UI",
-    "ui <- fluidPage(",
-    "tagList(",
-    'tags$html(lang = "en"), # Set the language of the page - important for accessibility',
-    "# Specify most recent fontawesome library - change version as needed",
-    'tags$style("@import url(https://use.fontawesome.com/releases/v6.6.0/css/all.css);"),',
-    "navbarPage(",
-    '    id = "intabset", # id used for jumping between tabs',
-    "    title = div(",
-    '        tags$a(img(src = "phs-logo-white.png", height = 40,',
-    '                   alt ="Go to Public Health Scotland (external site)"),',
-    '               href = "https://www.publichealthscotland.scot/",',
-    '               target = "_blank"), # PHS logo links to PHS website',
-    '    style = "position: relative; top: -10px;"),',
-    paste0("    windowTitle = ", '"', app_name, '"', ",", "# Title for browser tab"),
-    '    header = tags$head(includeCSS("www/styles.css"),  # CSS stylesheet',
-    '    tags$link(rel = "shortcut icon", href = "favicon_phs.ico") # Icon for browser tab',
-    "),\n"
+    ""
   )
 
   r_code <- paste(r_code, collapse = "\n")
--- inst/text/shiny/app.R
+++ inst/text/shiny/app.R
@@ -1,3 +1,21 @@
+# UI
+ui <- fluidPage(
+tagList(
+tags$html(lang = "en"), # Set the language of the page - important for accessibility
+# Specify most recent fontawesome library - change version as needed
+tags$style("@import url(https://use.fontawesome.com/releases/v6.6.0/css/all.css);"),
+navbarPage(
+    id = "intabset", # id used for jumping between tabs
+    title = div(
+        tags$a(img(src = "phs-logo-white.png", height = 40,
+                   alt ="Go to Public Health Scotland (external site)"),
+               href = "https://www.publichealthscotland.scot/",
+               target = "_blank"), # PHS logo links to PHS website
+    style = "position: relative; top: -10px;"),
+    windowTitle = "a",# Title for browser tab
+    header = tags$head(includeCSS("www/styles.css"),  # CSS stylesheet
+    tags$link(rel = "shortcut icon", href = "favicon_phs.ico") # Icon for browser tab
+),
 ##############################################.
 # INTRO PAGE ----
 ##############################################.
