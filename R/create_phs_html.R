7d4f656402caf8f34c56f87a2944f49efebc856c
matthh10 <matthew.hoyle@phs.scot>
2025-05-30 11:21:47 +0000

    new function `create_phs_html` to create qmd template for html report

---
+++ R/create_phs_html.R
@@ -0,0 +1,43 @@
+create_phs_html <- function(file_name = NULL,
+                              ext_name = "phs-html-quarto") {
+
+  if (is.null(file_name)) {
+    stop("You must provide a valid file_name")
+  }
+
+  # check for available extensions
+  stopifnot("Extension not in package" = ext_name %in% c("phs-html-quarto"))
+
+  # check for existing _extensions directory
+  if(!file.exists("_extensions")) dir.create("_extensions")
+  message("Created '_extensions' folder")
+
+  # create folder
+  if(!file.exists(paste0("_extensions/", ext_name))) dir.create(paste0("_extensions/", ext_name))
+
+  # copy from internals
+  file.copy(
+    from = system.file(paste0("extdata/_extensions/", ext_name), package = "hockeyutils"),
+    to = paste0("_extensions/"),
+    overwrite = TRUE,
+    recursive = TRUE,
+    copy.mode = TRUE
+  )
+
+  # logic check to make sure extension files were moved
+  n_files <- length(dir(paste0("_extensions/", ext_name)))
+
+  if(n_files >= 2){
+    message(paste(ext_name, "was installed to _extensions folder in current working directory."))
+  } else {
+    message("Extension appears not to have been created")
+  }
+
+  # create new qmd report based on skeleton
+  file.copy("_extensions/phs-html-quarto/phs-html-quarto-report-template.qmd",
+            paste0(file_name, ".qmd", collapse = ""))
+
+  # open the new file in the editor
+  file.edit(paste0(file_name, ".qmd", collapse = ""))
+
+}
