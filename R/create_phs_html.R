2c0ea4c9e9649d7af936db73f2401b55839e46aa
matthh10 <matthew.hoyle@phs.scot>
2025-06-09 12:57:30 +0000

    Change function to copy logo and css files from single location. Updated template with new locations.

--- R/create_phs_html.R
+++ R/create_phs_html.R
@@ -2,7 +2,6 @@
 #' @description Creates template `.qmd` file for a html report with PHS formatting.
 #'
 #' @param file_name Character string of name given to output `.qmd` file.
-#' @param ext_name Character string of extension folder name (default = "phs-html-quarto")
 #'
 #' @return Quarto document for html with PHS formatting.
 #' @export
@@ -11,13 +10,16 @@
 #' create_phs_html()
 #' }
 
-create_phs_html <- function(file_name = NULL, ext_name = "phs-html-quarto") {
+create_phs_html <- function(file_name = NULL) {
   if (is.null(file_name)) {
     stop("You must provide a valid file_name")
   }
 
-  # check for available extensions
-  stopifnot("Extension not in package" = ext_name %in% c("phs-html-quarto"))
+  # set extension name
+  ext_name = "phs-html-quarto"
+
+  # check for available extensions (future proofing for multiple templates)
+  stopifnot(ext_name %in% c("phs-html-quarto"))
 
   # check for existing _extensions directory
   if (!file.exists("_extensions")) {
@@ -30,7 +32,7 @@ create_phs_html <- function(file_name = NULL, ext_name = "phs-html-quarto") {
     dir.create(file.path("_extensions", ext_name))
   }
 
-  # copy from internals
+  # copy template from internals
   file.copy(
     from = system.file(
       file.path("extdata", "_extensions", ext_name),
@@ -40,6 +42,25 @@ create_phs_html <- function(file_name = NULL, ext_name = "phs-html-quarto") {
     overwrite = TRUE,
     recursive = TRUE,
     copy.mode = TRUE
+  )
+
+  # copy logo .png from internals
+  file.copy(
+    from = system.file(
+      file.path("images", "phs-logo.png"),
+      package = "phstemplates"
+    ),
+    to = file.path("_extensions", ext_name),
+    overwrite = TRUE,
+    recursive = TRUE,
+    copy.mode = TRUE
+  )
+
+  # add phs .css file
+  phstemplates::add_stylecss(
+    path = file.path("_extensions", ext_name),
+    shinycss = FALSE,
+    auto_open = FALSE
   )
 
   # logic check to make sure extension files were moved
